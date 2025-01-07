2cde03a39165670ad87108a8c01427f670823b2b
Luke Winikates <luke.winikates@gmail.com>
2025-01-06 23:31:32 -0800

    mqtt hello world

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,51 @@ package main
 
 import (
 	"fmt"
-	mqtt2 "github.com/eclipse/paho.mqtt.golang"
-	"log"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"os"
+	"time"
 )
 
-func main() {
-	options := mqtt2.NewClientOptions()
-	options.AddBroker(os.Getenv("MQTT_HOST"))
-	client := mqtt2.NewClient(options)
-	topic := os.Getenv("MQTT_TOPIC")
-	t := client.Subscribe(topic, 0, func(client mqtt2.Client, message mqtt2.Message) {
-		log.Println(message)
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
+func sub(client mqtt.Client) {
+	topic := "zigbee2mqtt/bridge/#"
+	token := client.Subscribe(topic, 1, func(client mqtt.Client, message mqtt.Message) {
+		fmt.Printf("Received message: %s from topic: %s\n", message.Payload(), message.Topic())
 	})
-	<-t.Done() // Can also use '<-t.Done()' in releases > 1.2.0
-	if t.Error() != nil {
-		fmt.Println(t.Error().Error())
-		os.Exit(1)
+	token.Wait()
+	fmt.Printf("Subscribed to topic %s", topic)
+}
+func main() {
+	//mqtt.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
+	//mqtt.CRITICAL = log.New(os.Stdout, "[CRIT] ", 0)
+	//mqtt.WARN = log.New(os.Stdout, "[WARN]  ", 0)
+	//mqtt.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
+
+	options := mqtt.NewClientOptions()
+	options.AddBroker(os.Getenv("MQTT_HOST"))
+	options.SetClientID("hogepiyo")
+	client := mqtt.NewClient(options)
+	options.SetDefaultPublishHandler(messagePubHandler)
+	options.OnConnect = connectHandler
+	options.OnConnectionLost = connectLostHandler
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
 	}
+	sub(client)
+
+	for {
+		time.Sleep(20 * time.Second)
+	}
+	//client.Disconnect(2500)
 }

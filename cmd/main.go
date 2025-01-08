7b73535d997648c49df3bcf38e1eeb4b8cfbfae0
Luke Winikates <luke.winikates@gmail.com>
2025-01-08 08:59:17 -0800

    spike: hello world, sending an on message

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"LukeWinikates/january-twenty-five/lib/devices"
 	"LukeWinikates/january-twenty-five/lib/payloads"
+	"encoding/json"
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"log"
@@ -47,13 +49,28 @@ func main() {
 		panic(token.Error())
 	}
 	subscribeDevices(client)
-	//subscribeLogging(client)
+	subscribeLightSignal(client)
 	//sub(client)
+
+	turnOnLight(client, os.Getenv("TEST_DEVICE_NAME"))
 
 	for {
 		time.Sleep(20 * time.Second)
 	}
 	//client.Disconnect(2500)
+}
+
+func turnOnLight(client mqtt.Client, deviceName string) {
+	message, err := json.Marshal(devices.OnMessage())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	topic := fmt.Sprintf("zigbee2mqtt/%s/set", deviceName)
+	publish := client.Publish(topic, 0, false, message)
+	if publish.Error() != nil {
+		fmt.Println(publish.Error().Error())
+	}
 }
 
 func subscribeDevices(client mqtt.Client) {
@@ -72,6 +89,15 @@ func subscribeDevices(client mqtt.Client) {
 		for _, device := range deviceList {
 			fmt.Println(device.FriendlyName)
 		}
+	})
+	token.Wait()
+	fmt.Printf("Subscribed to topic %s", topic)
+}
+func subscribeLightSignal(client mqtt.Client) {
+	topic := "zigbee2mqtt/Office Work Desk/set"
+	token := client.Subscribe(topic, 1, func(client mqtt.Client, message mqtt.Message) {
+		fmt.Printf("Received message: %s from topic: %s\n", message.Payload(), message.Topic())
+		fmt.Printf("%v", message)
 	})
 	token.Wait()
 	fmt.Printf("Subscribed to topic %s", topic)
---
+++ lib/devices/bulb.go
@@ -0,0 +1,62 @@
+package devices
+
+type Update struct {
+	InstalledVersion int    `json:"installed_version"`
+	LatestVersion    int    `json:"latest_version"`
+	State            string `json:"state"`
+}
+
+type LevelConfig struct {
+	OnLevel string `json:"on_level"`
+}
+
+type LightControl struct {
+	Brightness      int         `json:"brightness"`
+	ColorMode       string      `json:"color_mode"`
+	ColorTemp       int         `json:"color_temp"`
+	LevelConfig     LevelConfig `json:"level_config"`
+	LinkQuality     int         `json:"linkquality"`
+	State           string      `json:"state"`
+	Update          Update      `json:"update"`
+	UpdateAvailable bool        `json:"update_available"`
+}
+
+//
+
+func OnMessage() LightControl {
+	return LightControl{
+		Brightness: 254,
+		ColorMode:  "color_temp",
+		ColorTemp:  370,
+		LevelConfig: LevelConfig{
+			OnLevel: "previous",
+		},
+		LinkQuality: 36,
+		State:       "ON",
+		Update: Update{
+			InstalledVersion: 65554,
+			LatestVersion:    65554,
+			State:            "idle",
+		},
+		UpdateAvailable: false,
+	}
+}
+
+func OffMessage() LightControl {
+	return LightControl{
+		Brightness: 0,
+		ColorMode:  "color_temp",
+		ColorTemp:  370,
+		LevelConfig: LevelConfig{
+			OnLevel: "previous",
+		},
+		LinkQuality: 36,
+		State:       "OFF",
+		Update: Update{
+			InstalledVersion: 65554,
+			LatestVersion:    65554,
+			State:            "idle",
+		},
+		UpdateAvailable: false,
+	}
+}
